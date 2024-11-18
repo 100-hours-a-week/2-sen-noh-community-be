b51c8f7a4a2ddd9c0878476708f764f67379d36c
sein <si2238@naver.com>
2024-11-18 10:41:39 +0900

    feat: add cors
    cors 설정 추가

--- routes/middlewareRoutes.js
+++ routes/middlewareRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { timeoutMiddleware, timeoutHandler } = require('../middlewares/timeout');
 const cspMiddleware = require('../middlewares/csp');
+const corsMiddleware = require('../middlewares/cors');
 const { generalLimiter, getLimiter } = require('../middlewares/rateLimit');
 
 const router = express.Router();
@@ -8,6 +9,7 @@ const router = express.Router();
 router.use(timeoutMiddleware);
 router.use(timeoutHandler);
 router.use(cspMiddleware);
+router.use(corsMiddleware);
 
 router.use(generalLimiter);
 
