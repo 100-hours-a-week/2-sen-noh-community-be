87c3ba0e0a3b637b13cdf8bc27ce72fa429a8ce5
sein <si2238@naver.com>
2024-11-19 16:58:46 +0900

    edit: data
    데이터 내용 수정
    cors 도메인 수정

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,10 @@
 const cors = require('cors');
 
 const corsMiddleware = cors({
-    origin: '*',  // 클라이언트가 요청을 보내는 도메인과 포트
-    methods: ['GET', 'POST',  'DELETE', 'PATCH', ],  // 허용할 HTTP 메소드
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, 
-})
+    origin: '*', // 클라이언트가 요청을 보내는 도메인과 포트
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'], // 허용할 HTTP 메소드
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+});
 
 module.exports = corsMiddleware;
