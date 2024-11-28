ed87decfac7d8dd409572fed197b70e26f5e5a0d
sein <si2238@naver.com>
2024-11-28 16:54:41 +0900

    fix: auto create image upload dir
    이미지 업로드 폴더 자동 생성

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,11 +1,25 @@
 import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        const uploadPath = 'uploads/';
+
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true }); // 중간 폴더도 생성 가능하도록 recursive: true 설정
+        }
+
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        const fileExtension = path.extname(file.originalname); // 파일 확장자를 분리
+        const fileNameWithoutExt = path.basename(
+            file.originalname,
+            fileExtension,
+        );
+        const newFileName = `${fileNameWithoutExt}-${Date.now()}${fileExtension}`; // 날짜와 확장자 포함
+        cb(null, newFileName);
     },
 });
 
