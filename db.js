4efce09a2cdbccadd1cc89ba3d0a059871695011
sein <si2238@naver.com>
2024-12-02 13:49:19 +0900

    feat: set up db
    데이터베이스 연결

---
+++ db.js
@@ -0,0 +1,18 @@
+// db/database.js
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// MySQL 연결 풀 설정
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+export default pool;
