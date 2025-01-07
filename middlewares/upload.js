676d9899d2bfa5cd03305bdd6c018e8f06645cbe
sein <si2238@naver.com>
2025-01-07 15:59:15 +0900

    chore: change image file naming - UUID
    이미지 파일 네이밍 방식 변경
    UUID 사용

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,24 +1,21 @@
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         const uploadPath = 'uploads/';
 
         if (!fs.existsSync(uploadPath)) {
-            fs.mkdirSync(uploadPath, { recursive: true }); // 중간 폴더도 생성 가능하도록 recursive: true 설정
+            fs.mkdirSync(uploadPath, { recursive: true });
         }
 
         cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         const fileExtension = path.extname(file.originalname); // 파일 확장자를 분리
-        const fileNameWithoutExt = path.basename(
-            file.originalname,
-            fileExtension,
-        );
-        const newFileName = `${fileNameWithoutExt}-${Date.now()}${fileExtension}`; // 날짜와 확장자 포함
+        const newFileName = `${uuidv4()}${fileExtension}`;
         cb(null, newFileName);
     },
 });
