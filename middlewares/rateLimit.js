4283d7001975aa4c778605cd5e8e6d14022dcf91
sein <si2238@naver.com>
2025-02-07 16:36:53 +0900

    chore: adjust rate limit setting for load test
    로드테스트를 위해 ratelimit 조정

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,13 +2,13 @@ import rateLimit from 'express-rate-limit';
 
 export const generalLimiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 100,
+    max: 10000,
     message: '이 ip로부터 제한 시간에 너무 많은 요청이 왔다, 10분 뒤 다시 요청',
     skip: req => req.method === 'GET',
 });
 
 export const getLimiter = rateLimit({
     windowMs: 5 * 60 * 1000,
-    max: 200,
+    max: 20000,
     message: '이 ip로부터 너무 많은 요청이 왔다, 5분 뒤 다시 요청해주새요',
 });
